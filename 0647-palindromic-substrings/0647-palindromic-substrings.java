6f2b2043eba167cf233295df9579f4e109e03fb7
Syed Ateef <syedatifsyed07@gmail.com>
2026-09-18 22:22:12 +0530

    Time: 802 ms (13.56%), Space: 43 MB (55.4%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int n=s.length();
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(ispolindrom(s,i,j)){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    private boolean ispolindrom(String s,int i,int j){
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
