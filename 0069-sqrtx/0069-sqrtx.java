366a6b1167bf3648a2651bd5656a6ed25eabbc2f
Syed Ateef <syedatifsyed07@gmail.com>
2026-09-22 01:06:12 +0530

    Time: 1 ms (99.63%), Space: 42.7 MB (23.93%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int mySqrt(int x) {
+    if(x==0 || x==1) return x;
+    int l=0;
+    int r=x/2;
+    while(l<=r){
+        int mid=l+(r-l)/2;
+        long sq=(long)mid*mid;
+        if(sq == x) return mid;
+        else if(sq < x) l=mid+1;
+        else r=mid-1;
+    }
+    return r;
+    }
+}
